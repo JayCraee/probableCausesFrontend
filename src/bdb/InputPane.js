08d5c53995ec3c6e090755cea08df57285e63197
Ross Tooley <rj_tooley@hotmail.co.uk>
2019-02-09 21:51:00 +0000

    Changed InputPane to hold just DiagramPane
    Before it held pure konva stage and shapes within

--- src/bdb/InputPane.js
+++ src/bdb/InputPane.js
@@ -1,24 +1,13 @@
 import React, {Component} from 'react';
-import {Stage, Layer} from 'react-konva';
-import StyledEllipse from './shapes/StyledEllipse';
-import StyledRect from './shapes/StyledRect';
-import StyledDiamond from './shapes/StyledDiamond';
-import StyledParallelogram from './shapes/StyledParallelogram';
+import DiagramPane from './DiagramPane';
 
 class InputPane extends Component {
   render() {
-    let width = 500;
-    let height = 500;
     return (
-      <Stage width={width} height={height}>
-        <Layer>
-          <StyledEllipse/>
-          <StyledRect/>
-          <StyledParallelogram/>
-          <StyledDiamond/>
-        </Layer>
-      </Stage>
-    )
+      <div>
+        <DiagramPane/>
+      </div>
+    );
   }
 }
 
