bd4634328f0f8da5cf0a7a5cafa29c46b1973f69
Ross Tooley <rj_tooley@hotmail.co.uk>
2019-02-09 23:41:49 +0000

    Added wrap to expression
    Now has option to add a line wrap that shifts the text up so that a second line looks good inside an ellipse

--- src/bdb/shapes/Expression.js
+++ src/bdb/shapes/Expression.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Group, Text } from 'react-konva';
 import StyledEllipse from './StyledEllipse';
 
+
 class Expression extends Component {
   render() {
     let x = this.props.x;
@@ -9,16 +10,27 @@ class Expression extends Component {
     let width = this.props.width;
     let height = this.props.height;
     let text = this.props.expression;
+    let wrap = Boolean(this.props.wrap);
+
+    let textOffset=0.1;
 
     let ellipseX = x + Math.floor(width/2);
     let ellipseY = y + Math.floor(height/2);
-    let textX = x + Math.floor(width*0.1);
-    let textY = y + Math.floor(height*0.3);
+    let textX = x + Math.floor(width*textOffset);
+    let textY;
+
+    let textWidth = Math.floor(width*(1-2*textOffset));
+
+    if (wrap) {
+      textY = y + Math.floor(height*0.16);
+    } else {
+      textY = y + Math.floor(height*0.3);
+    }
 
     return (
       <Group>
         <StyledEllipse x={ellipseX} y={ellipseY} width={width} height={height}/>
-        <Text x={textX} y={textY} text={text} fontSize={20}/>
+        <Text x={textX} y={textY} text={text} fontSize={20} width={textWidth}/>
       </Group>
     )
   }
