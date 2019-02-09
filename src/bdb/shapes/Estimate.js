347a9b97c1eaf94a8568d5dc0539a4e9f967ce99
Ross Tooley <rj_tooley@hotmail.co.uk>
2019-02-09 23:42:22 +0000

    Improved text positioning of Estimate
    Now centralised text positioning

--- src/bdb/shapes/Estimate.js
+++ src/bdb/shapes/Estimate.js
@@ -7,12 +7,14 @@ class Estimate extends Component {
   render() {
     let x=50;
     let y=50;
-    let width=200;
+    let width=400;
     let height=100;
     let text="ESTIMATE";
 
-    let textHorizontalOffset=0.15;
-    let textVerticalOffset=0.15;
+    let textHorizontalIndent=0.06;
+    let textHorizontalOffset=0.1;
+    let textVerticalOffset=0.13;
+
 
     let expressionHorizontalOffset=0.07;
     let expressionVerticalOffset=0.45;
@@ -28,14 +30,15 @@ class Estimate extends Component {
 
     let parallelogramWidth=width;
     let parallelogramHeight=height;
+    let textWidth = Math.floor(width*(1-2*textHorizontalIndent));
     let expressionWidth = Math.floor(width*expressionHorizontalScale);
     let expressionHeight = Math.floor(height*expressionVerticalScale);
 
     return (
       <Group>
         <StyledParallelogram x={parallelogramX} y={parallelogramY} width={parallelogramWidth} height={parallelogramHeight}/>
-        <Text x={textX} y={textY} text={text} fontSize={24} fontStyle="bold" fill="white"/>
-        <Expression x={expressionX} y={expressionY} width={expressionWidth} height={expressionHeight} expression="Hello, world"/>
+        <Text x={textX} y={textY} text={text} fontSize={24} fontStyle="bold" fill="white" width={textWidth} align="center"/>
+        <Expression x={expressionX} y={expressionY} width={expressionWidth} height={expressionHeight} expression="HelloHHHHiiiiHHHHHH,worldddddddddd" wrap/>
       </Group>
     );
   }
