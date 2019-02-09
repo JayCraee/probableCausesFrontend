f6ee90e8c6a0a49b3dbdf9ea93cda0ee5051bb9a
Ross Tooley <rj_tooley@hotmail.co.uk>
2019-02-09 21:51:25 +0000

    Updated QueryPane to hold InputPane

--- src/bdb/QueryPane.js
+++ src/bdb/QueryPane.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
+import InputPane from "./InputPane";
 
 class QueryPane extends Component {
-  
+  render() {
+    return (
+      <div>
+        <InputPane/>
+      </div>
+    )
+  }
 }
 
 export default QueryPane;
