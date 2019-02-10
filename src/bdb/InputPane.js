62808b6b2adf1466434b18c833bb01ce6cccebc3
Ross Tooley <rj_tooley@hotmail.co.uk>
2019-02-10 10:45:55 +0000

    Added SideMenuPane to InputPane
    Created table to hold both SideMenu and Diagram, scales each according to how large InputPane is

--- src/bdb/InputPane.js
+++ src/bdb/InputPane.js
@@ -1,12 +1,24 @@
 import React, {Component} from 'react';
 import DiagramPane from './DiagramPane';
+import SideMenuPane from './SideMenuPane';
 
 class InputPane extends Component {
   render() {
+    const width = 1500;
+    const height = 500;
+
+    const sideMenuWidth = Math.floor(width/6);
+    const diagramWidth = Math.floor(width*5/6);
+
     return (
-      <div>
-        <DiagramPane/>
-      </div>
+      <table id="input-pane-table">
+        <tbody>
+          <tr>
+            <td><SideMenuPane width={sideMenuWidth} height={height}/></td>
+            <td><DiagramPane width={diagramWidth} height={height}/></td>
+          </tr>
+        </tbody>
+      </table>
     );
   }
 }
