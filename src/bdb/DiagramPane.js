52c1235a6502ac2d88b7c99fc69621720a59b33a
Ross Tooley <rj_tooley@hotmail.co.uk>
2019-02-10 10:44:20 +0000

    Exposed DiagramPane height and width as props

--- src/bdb/DiagramPane.js
+++ src/bdb/DiagramPane.js
@@ -4,8 +4,8 @@ import Estimate from './shapes/Estimate'
 
 class DiagramPane extends Component {
   render() {
-    let width = 500;
-    let height = 500;
+    let width = this.props.width;
+    let height = this.props.height;
     return (
       <Stage width={width} height={height}>
         <Layer>
