0d7eaefc27459ae37a8b5082a413fa02c78c2f04
Ross Tooley <rj_tooley@hotmail.co.uk>
2019-02-12 22:50:41 +0000

    Created the function that renders the diagram in different ways depending on the state of the query

--- src/bdb/DiagramPane.js
+++ src/bdb/DiagramPane.js
@@ -1,52 +1,134 @@
 import React, { Component } from 'react';
-import {Stage, Layer, Group} from 'react-konva';
+import {Stage, Layer, Group, Arrow} from 'react-konva';
 import EstimateQuery from './data/EstimateQuery';
 import SimilarityExpression from "./data/SimilarityExpression";
+import StyledEstimate from "./shapes/StyledEstimate";
+import FixedFreeRect from "./shapes/FixedFreeRect";
+import StyledRect from "./shapes/StyledRect";
+import OrderBy from "./shapes/OrderBy";
+import Limit from "./shapes/Limit";
 
 class DiagramPane extends Component {
   renderDiagram(query) {
+    const row1X = 100;
+    const rowY = 30;
+    const row2X = 330;
+
+    const arrowToRow1X = 150;
+    const arrowToRowY = 130;
+    const arrowToRow1Points = [0,0,65,65];
+    const arrowColour = "black";
+    const arrowToRow2X = 370;
+    const arrowToRow2Points = [0,0,-65,65];
+
+    const orderByX = 180;
+    const orderByY = 350;
+
+    const arrowToOrderByX = 255;
+    const arrowToOrderByY = 300;
+    const arrowBetweenDiamondsPoints = [0,0,0,50];
+
+    const limitX = 180;
+    const limitY = 470;
+
+    const arrowToLimitX = 255;
+    const arrowToLimitY = 420;
+
     let diagram;
     if (query instanceof EstimateQuery) {
       if (query.expressionChosen) {
         if (query.expression instanceof SimilarityExpression) {
           let estimate;
           if (query.contextChosen) {
-            //TODO: draw estimate with expression
+            // draw estimate with expression
+            estimate = <StyledEstimate expression={query.expressionName}/>;
           } else {
-            //TODO: draw estimate with expression plus showing that they need to complete query
+            // draw estimate with expression plus showing that they need to complete query
+            estimate = <StyledEstimate expression={query.expressionName + " todo "}/>
           }
           let row1;
           if (query.row1Chosen) {
-            if (query.row1Fixed()) {
-              //TODO: draw row1 as fixed
+            if (query.row1Fixed) {
+              // draw row1 as fixed
+              row1 = <StyledRect x={row1X} y={rowY} text={"SINGLE ROW"}/>
             } else {
-              //TODO: draw row1 as free
+              // draw row1 as free
+              row1 = <StyledRect x={row1X} y={rowY} text={"EVERY ROW"}/>
             }
           } else {
-            //TODO: draw row1 as an option
+            // draw row1 as an option
+            row1 = <StyledRect x={row1X} y={rowY}/>
           }
-          let arrowToRow1;
           //TODO: draw arrow to row1
+          let arrowToRow1= <Arrow
+            x={arrowToRow1X}
+            y={arrowToRowY}
+            points={arrowToRow1Points}
+            fill={arrowColour}
+            stroke={arrowColour}/>;
+
           let row2;
           if (query.row2Chosen) {
-            if (query.row2Fixed()) {
-              //TODO: draw row2 as fixed
+            if (query.row2Fixed) {
+              // draw row2 as fixed
+              row2 = <StyledRect x={row2X} y={rowY} text={"SINGLE ROW"}/>
             } else {
               //TODO: draw row2 as free
+              row2 = <StyledRect x={row2X} y={rowY} text={"EVERY ROW"}/>
             }
           } else {
-            //TODO: draw row2 as an option
+            // draw row2 as an option
+            row2 = <StyledRect x={row2X} y={rowY}/>
           }
-          let arrowToRow2;
-          //TODO: draw arrow to row2
-          let orderBy;
-          //TODO: draw order by
-          let arrowToOrderBy;
-          //TODO: draw arrow to order by
-          let limit;
-          //TODO: draw limit
-          let arrowToLimit;
-          //TODO: draw arrow to limit
+
+          // draw arrow to row2
+          let arrowToRow2 = <Arrow
+            x={arrowToRow2X}
+            y={arrowToRowY}
+            points={arrowToRow2Points}
+            fill={arrowColour}
+            stroke={arrowColour}
+          />;
+
+          let optionalGroup;
+
+          if (query.rowsComplete) {
+
+            // draw order by
+            let orderBy = <OrderBy x={orderByX} y={orderByY}/>;
+
+            // draw arrow to order by
+            let arrowToOrderBy = <Arrow
+              x={arrowToOrderByX}
+              y={arrowToOrderByY}
+              points={arrowBetweenDiamondsPoints}
+              fill={arrowColour}
+              stroke={arrowColour}
+            />;
+
+            // draw limit
+            let limit = <Limit x={limitX} y={limitY}/>;
+
+            // draw arrow to limit
+            let arrowToLimit = <Arrow
+              x={arrowToLimitX}
+              y={arrowToLimitY}
+              points={arrowBetweenDiamondsPoints}
+              fill={arrowColour}
+              stroke={arrowColour}
+            />;
+
+            optionalGroup = (
+              <Group>
+                {orderBy}
+                {arrowToOrderBy}
+                {limit}
+                {arrowToLimit}
+              </Group>
+            );
+          }
+
+
           diagram = (
             <Group>
               {estimate}
@@ -54,15 +136,13 @@ class DiagramPane extends Component {
               {arrowToRow1}
               {row2}
               {arrowToRow2}
-              {orderBy}
-              {arrowToOrderBy}
-              {limit}
-              {arrowToLimit}
+              {optionalGroup}
             </Group>
           )
         }
       } else {
-        //TODO: draw estimate without expression
+        // draw estimate without expression
+        diagram = <StyledEstimate/>;
       }
     }
     return diagram;
@@ -73,7 +153,11 @@ class DiagramPane extends Component {
     const width = this.props.width;
     const height = this.props.height;
 
-    let query;
+    let query = new EstimateQuery();
+    query.expression = new SimilarityExpression();
+    query.context = "column1";
+    query.row1Fixed = true;
+    query.row2Fixed = false;
 
     return (
       <Stage width={width} height={height}>
@@ -81,7 +165,7 @@ class DiagramPane extends Component {
           {this.renderDiagram(query)}
         </Layer>
       </Stage>
-    );
+    )
   }
 }
 
