945271b4705af3a7292c870dd878d06ce42abb4e
Ross Tooley <rj_tooley@hotmail.co.uk>
2019-02-13 16:45:56 +0000

    QueryPane now holds query and passes down an onClick function to choose the expression.

--- src/bdb/QueryPane.js
+++ src/bdb/QueryPane.js
@@ -1,13 +1,46 @@
 import React, { Component } from 'react';
 import InputPane from "./InputPane";
+import EstimateQuery from "./data/EstimateQuery";
+import SimilarityExpression from "./data/SimilarityExpression";
+import UnsupportedInputPaneOperationError from "./error/UnsupportedInputPaneOperationError";
+import UnsupportedExpressionError from "./error/UnsupportedExpressionError";
 
 class QueryPane extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: new EstimateQuery(),
+    }
+  }
+
+  handleInputPaneClick(operationID, ...args) {
+    switch (operationID) {
+      case 0:
+        //choose expression
+        let expressionNum = args[0];
+        switch (expressionNum) {
+          case 0:
+            let query = this.state.query;
+            query.expression = new SimilarityExpression();
+            this.setState({
+              query: query,
+            });
+            break;
+          default:
+            throw new UnsupportedExpressionError(expressionNum);
+        }
+        break;
+      default:
+        throw new UnsupportedInputPaneOperationError(operationID);
+    }
+  }
+
   render() {
     return (
       <table id="query-pane-table">
         <tbody>
           <tr>
-            <td><InputPane/></td>
+            <td><InputPane onClick={(operationID, ...args) => this.handleInputPaneClick(operationID, ...args)} query={this.state.query}/></td>
           </tr>
         </tbody>
       </table>
