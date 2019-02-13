c013d346a6e6782e1371b0a381c589a705d46907
Ross Tooley <rj_tooley@hotmail.co.uk>
2019-02-13 16:44:55 +0000

    currentlySelected state lifted to InputPane, onClick handlers for shapes on diagram passed down, onClicks for query updates passed through

--- src/bdb/InputPane.js
+++ src/bdb/InputPane.js
@@ -1,8 +1,52 @@
 import React, {Component} from 'react';
 import DiagramPane from './DiagramPane';
 import SideMenuPane from './SideMenuPane';
+import UnsupportedSideMenuError from "./error/UnsupportedSideMenuError";
 
 class InputPane extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentlySelected: 5,
+    }
+  }
+
+  handleChooseExpressionClick(expressionNumClicked) {
+    this.props.onClick(0, expressionNumClicked);
+  }
+
+  getSideMenuHandler(currentlySelected) {
+    switch (currentlySelected) {
+      case 0:
+        return (() => {});
+      case 1:
+        return (expressionNumClicked => {this.props.onClick(0, expressionNumClicked)});
+      case 2:
+        //TODO
+        return (() => {});
+      case 3:
+        //TODO
+        return (() => {});
+      case 4:
+        //TODO
+        return (() => {});
+      case 5:
+        //TODO
+        return (() => {});
+      case 6:
+        //TODO
+        return (() => {});
+      default:
+        throw new UnsupportedSideMenuError(currentlySelected);
+    }
+  }
+
+  handleDiagramClick(currentlySelected) {
+    this.setState({
+      currentlySelected: currentlySelected,
+    })
+  }
+
   render() {
     const sideMenuWidth = 250;
     const diagramWidth = 500;
@@ -13,8 +57,19 @@ class InputPane extends Component {
       <table>
         <tbody>
           <tr>
-            <td><SideMenuPane width={sideMenuWidth} height={height}/></td>
-            <td><DiagramPane width={diagramWidth} height={height}/></td>
+            <td><SideMenuPane
+              width={sideMenuWidth}
+              height={height}
+              currentlySelected={this.state.currentlySelected}
+              onClick={this.getSideMenuHandler(this.state.currentlySelected)}
+            />
+            </td>
+            <td><DiagramPane
+              width={diagramWidth}
+              height={height}
+              onClick={i => this.handleDiagramClick(i)}
+              query={this.props.query}/>
+            </td>
           </tr>
         </tbody>
       </table>
