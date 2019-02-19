876320801a079a9c232441a3c8e39d9cc12b8129
Ross Tooley <rj_tooley@hotmail.co.uk>
2019-02-19 15:44:14 +0000

    Added CORRELATION as a statistic

--- src/bdb/BqlStudio.js
+++ src/bdb/BqlStudio.js
@@ -5,7 +5,7 @@ import {withRouter} from "react-router-dom";
 class BqlStudio extends Component {
 
   static getExpressions() {
-    return ['SIMILARITY'];
+    return ['SIMILARITY', 'CORRELATION'];
   }
 
   render() {
