c01ebebbdc74146646ece0d0df39830ca1c066d0
Csitari Nora <csitari.nora@gmail.com>
2019-02-22 14:22:14 +0000

    http request in QueryPane, OutputPane created

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-import AppNavbar from './AppNavbar';
-import { Link } from 'react-router-dom';
-import { Button, Container } from 'reactstrap';
+import BqlStudio from './bdb/BqlStudio';
 
 class Home extends Component {
   render() {
     return (
-      <div>
-        <AppNavbar/>
-        <Container fluid>
-          <Button color="link"><Link to="/groups">Manage JUG Tour</Link></Button>
-        </Container>
-      </div>
+      <BqlStudio/>
     );
   }
 }
---
+++ src/bdb/OutputPane.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import Request from 'react-http-request';
+
+class OutputPane extends Component {
+    
+
+    render() {
+        const divStyle = {
+          height: 300+'px',
+          width: 750+'px',
+          backgroundColor: '#7EBAD9',
+        };
+    
+        return (
+          <div style={divStyle}>
+            
+          </div>
+        );
+    
+      }
+    }
+    
+export default OutputPane;
