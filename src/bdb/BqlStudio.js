9246a3f4869895e7e08568ee07f9240cc9679eb3
Ross Tooley <rj_tooley@hotmail.co.uk>
2019-02-24 20:24:24 +0000

    Allowed multiple queries

--- src/bdb/BqlStudio.js
+++ src/bdb/BqlStudio.js
@@ -1,16 +1,33 @@
 import React, {Component} from 'react';
+import {Button} from 'reactstrap';
 import QueryPane from './QueryPane';
 import {withRouter} from "react-router-dom";
 
 class BqlStudio extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      numQueries: 1,
+    }
+  }
+
 
   static getExpressions() {
     return ['SIMILARITY', 'CORRELATION'];
   }
 
   render() {
+    let rows = [];
+    for (let i = 0; i < this.state.numQueries; ++i) {
+      rows.push(<tr><td id='query'><QueryPane/></td></tr>)
+    }
+    rows.push(<tr><td id='query'><Button onClick={()=>this.setState({numQueries: this.state.numQueries+1})}>+</Button></td></tr>);
     return (
-      <QueryPane/>
+      <table width="100%">
+        <tbody>
+          {rows}
+        </tbody>
+      </table>
     );
   }
 }
