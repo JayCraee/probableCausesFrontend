b02701e35c1ec582a915ab029a353f7e26b82654
Csitari Nora <csitari.nora@gmail.com>
2019-03-03 21:23:15 +0000

    barchart value colors fixed, red border deleted

--- src/bdb/BarGroup.js
+++ src/bdb/BarGroup.js
@@ -3,10 +3,7 @@ import React, {Component} from 'react';
 class BarGroup extends Component{
 
     render() {
-        let labelLimit = 130
         let barPadding = 2
-        //let barColour = '#329fff'
-        //let barColour = '#348AA7'
         let barColour = '#1c6ca1'
         let widthScale = d => d * 500
     
@@ -18,7 +15,7 @@ class BarGroup extends Component{
             <g className="bar-group">
             <text className="name-label" x="-6" y={yMid} alignmentBaseline="middle" >{props.d.name}</text>
             <rect y={barPadding * 0.15} width={width} height={props.barHeight - barPadding} fill={barColour} />
-            <text className={width<labelLimit ? "value-label-dark": "value-label-light"} x={width<labelLimit ? width+8: width-8} y={yMid} alignmentBaseline="middle" >{props.d.value}</text>
+            <text className={"value-label-dark"} x={width+8} y={yMid} alignmentBaseline="middle" >{props.d.value}</text>
             </g>
         )
     }
